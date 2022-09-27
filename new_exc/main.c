7e16428db8179c97b3fa8fa6eb95dc29fe8c529a
Netvoiangel <110711364+Netvoiangel@users.noreply.github.com>
2022-09-27 14:52:21 +0300

    first commit

--- new_exc/main.c
+++ new_exc/main.c
@@ -1,14 +1,23 @@
-//
-//  main.c
-//  new_exc
-//
-//  Created by timur on 27.09.2022.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void deleteProbel(char *str)
+{
+    printf("%s\n", str);
+    char str_out[sizeof(str)];
+    int i = 0, j = 0;
+    while (str[i] != '\0') {
+        if ((int)(str[i]) != 32) {
+            str_out[j] = str[i];
+            j ++;
+        }
+        i ++;
+    }
+    printf("%s\n", str_out);
+}
+
+
+int main()
+{
+    deleteProbel("  1 2  44");
     return 0;
 }
